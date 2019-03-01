c107d057f6ac20c235eb10b8b186dff2e68963f1
christ0208 <christopherlimawan@gmail.com>
2019-03-01 13:12:15 +0700

    Added Register with Firebase Auth

--- app/src/main/java/edu/bluejack181/thedoctorcheckups/RegisterActivity.kt
+++ app/src/main/java/edu/bluejack181/thedoctorcheckups/RegisterActivity.kt
@@ -9,17 +9,60 @@ package edu.bluejack181.thedoctorcheckups
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val mDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val mDatabaseReference: DatabaseReference? = mDatabase.getReference("users")
+    var gender:String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        gender_group.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
+            val radio: RadioButton = findViewById(checkedId)
+//            Toast.makeText(applicationContext, radio.text, Toast.LENGTH_SHORT).show()
+            gender = radio.text.toString()
+        })
+
         btn_register.setOnClickListener{
-            Toast.makeText(this, "Testing Register", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "Testing Register", Toast.LENGTH_SHORT).show()
+            val name = txt_name.text.toString()
+            val email = txt_email.text.toString()
+            val address = txt_address.text.toString()
+            val password = txt_password.text.toString()
+            val cpassword = txt_confirm_password.text.toString()
+
+            if(password.length < 6){
+                Toast.makeText(applicationContext, "Password must be more than 6 characters", Toast.LENGTH_SHORT).show()
+            }
+            else if(password != cpassword){
+                Toast.makeText(applicationContext, "Password and Confirm password must be the same", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this){task->
+                            if(task.isSuccessful){
+                                val userId = mAuth.currentUser!!.uid
+                                val currentUser = mDatabaseReference!!.child(userId)
+                                currentUser.child("name").setValue(name)
+                                currentUser.child("email").setValue(email)
+                                currentUser.child("address").setValue(address)
+                                currentUser.child("gender").setValue(gender)
+                            }
+                            else{
+                                Toast.makeText(applicationContext, "Something's wrong with server. Please try again later.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+            }
         }
 
         btn_login.setOnClickListener{
